4201d0341f5793e42533be39fd49c3b011efc92d
Erik Lundquist <erikmlundquist@gmail.com>
2015-04-20 15:23:17 -0400

    Update main.cpp
    fixed a copy-paste disaster I had

--- main.cpp
+++ main.cpp
@@ -1,5 +1,3 @@
-
-//I copied this from our class code -Erik
 #ifdef __APPLE__
 #  include <GL/glew.h>
 #  include <GL/freeglut.h>
@@ -10,21 +8,26 @@
 #  include <GL/glext.h>
 #pragma comment(lib, "glew32.lib") 
 #endif
+//just copied the above from in class files -Erik
 
+#include <fstream>
 
+//globals
+int data_array[7][300];
+
+//reads in the csv
 void read_file(){
-  ifstream csv_file ("Bicycle-Parking.csv"); 
+  ifsteam csv_file ("Bicycle-PArking.csv");
   string value;
   int array_x = 0;
   int array_y = 0;
-  int data_array[7][300];
-  
-  while(!file.eof()){
-  getline ( csv_file, value, ',' );
-  data_array[array_x][array_y] = value;
-  array_x++;
-  array_x = array_x % 7;
-  if(array_x == 0) array_y++;
+
+  while(!csv_file.eof()){
+    getline ( csv_file, value, ',' );
+    data_array[array_x][array_y] = value;
+    array_x++;
+    array_x = array_x % 7;
+    if(array_x == 0) array_y++;
   }
 }
 
